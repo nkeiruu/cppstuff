b6150fca4de04a3742f892c8683f03898fd9787e
nkeiruu <nkeiruubadike@gmail.com>
2021-12-28 00:32:35 -0500

    put method prototypes

--- LinkedList/Node.hpp
+++ LinkedList/Node.hpp
@@ -1,4 +1,11 @@
 class Node{
+    private:
     Node* next; //pointer to next node
     int data;
+    
+    public:
+    Node(int);// constructor
+    Node();
+    Node(const Node&);
+    ~Node();
 }
